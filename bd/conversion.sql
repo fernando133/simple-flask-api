9155a027408f5fdb6bde94cd11a7e535426b65eb
Fernando Gualberto <desenvolvimento@inlinetech.com.br>
2019-11-28 11:56:35 -0300

    sql update

--- bd/conversion.sql
+++ bd/conversion.sql
@@ -1,3 +1,3 @@
 SELECT
-	(SELECT access_amount FROM flask.campaing) /
-	(SELECT count(id) FROM flask.lead where origin='form') * 100 as CONVERSION;
+	(SELECT access_amount FROM campaing) /
+	(SELECT count(id) FROM lead where origin='form') * 100 as CONVERSION;
