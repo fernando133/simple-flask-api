1908e9a38ce4031c8514dbbaaa4e37b86478011e
Fernando Gualberto <desenvolvimento@inlinetech.com.br>
2019-12-14 14:22:16 -0300

    implementando redirecionamento para pagamento

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import urllib2
 import json
 from helpers.lead_helper import LeadHelper
 import os
-from flask import Flask, flash, redirect, url_for
+from flask import Flask, flash, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
+UPLOAD_FOLDER = '/home/inline/files'
 base_url = 'https://api.telegram.org/'
 TOKEN = '1234'
 
@@ -21,55 +21,68 @@ CORS(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def authorize(_token):
-	if _token == TOKEN:
-		return True
-	else:
-		return False
+    if _token == TOKEN:
+        return True
+    else:
+        return False
+
+def link_pagamento(foco_aulas):
+    if foco_aulas == "Ensino Superior":
+        return "https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=170979160-35cea3f4-9493-4f79-8c10-28d2f49240c0"
+    elif foco_aulas == "Ensino Médio":
+        return "https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=170979160-8c3b3867-c461-4b87-8706-c50dad81f746"
+    elif foco_aulas == "Ensino Fundamental":
+        return "https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=170979160-ab29a5f6-b118-45e2-b768-83dde14a1a70"
+
 
 @app.route("/lead/<name>/<email>/<necessity>/<enterprise>/<role>/<state>/<city>/<phone>/<celphone>/<origin>/<alert>", methods=['GET'])
 def add_lead(name, email, necessity, enterprise, role, state, city, phone, celphone, origin, alert):
-	lh = LeadHelper()
-	return lh.insert_lead(name, email, necessity, enterprise, role, state, city, phone, celphone, origin, alert)
+    lh = LeadHelper()
+    return lh.insert_lead(name, email, necessity, enterprise, role, state, city, phone, celphone, origin, alert)
 
 @app.route("/campaing/<origin>", methods=['GET'])
 def compute_access(origin):
-	lh = LeadHelper()
-	return lh.compute_access(origin)
+    lh = LeadHelper()
+    return lh.compute_access(origin)
 
 @app.route('/inscricao', methods=['POST'])
 def nova_inscricao():
-	data = request.form
-	historico_escolar = None
-	diploma = None
-	try:
-		if 'historico_escolar' in request.files:
-			historico_escolar = request.files['historico_escolar']
-			if historico_escolar.filename != '':
-				historico_escolar.save(os.path.join('/home/inline/files', historico_escolar.filename))
+    data = request.form
+    historico_escolar = None
+    diploma = None
+    try:
+        if 'historico_escolar' in request.files:
+            historico_escolar = request.files['historico_escolar']
+            if historico_escolar.filename != '':
+                historico_escolar.save(os.path.join(app.config['UPLOAD_FOLDER'], historico_escolar.filename))
 
-		if 'diploma' in request.files:
-			diploma = request.files['diploma']
-			if diploma.filename != '':
-				diploma.save(os.path.join('/home/inline/files', diploma.filename))
-	except:
-		return 500
+        if 'diploma' in request.files:
+            diploma = request.files['diploma']
+            if diploma.filename != '':
+                diploma.save(os.path.join(app.config['UPLOAD_FOLDER'], diploma.filename))
+    except:
+        return 500
 
-	data = json.dumps(data)
-	data = json.loads(data)
-	lh = LeadHelper()
-	return lh.nova_inscricao(data, historico_escolar.filename, diploma.filename)
+    data = json.dumps(data)
+    data = json.loads(data)
+    lh = LeadHelper()
+    nova_iscricao = lh.nova_inscricao(data, historico_escolar.filename, diploma.filename)
+    if nova_inscricao:
+        return render_template('inscricao-sucesso.html', link = link_pagamento(data['foco_aulas']))
+    else:
+        return render_template('inscricao-erro.html')
 
 @app.route("/broadcast", methods=['POST'])
 def broadcast_post():
-	data = request.json
-	json_str = json.dumps(data)
-	resp = json.loads(json_str)
-	_token, bot, chat_id, msg = resp['token'], resp['bot'], resp['chat_id'], resp['msg']
-	
-	if authorize(_token):
-		contents = urllib2.urlopen(base_url+bot+"/sendMessage?chat_id="+chat_id+"&text="+msg).read()
-	else:
-		return "invalid token"
+    data = request.json
+    json_str = json.dumps(data)
+    resp = json.loads(json_str)
+    _token, bot, chat_id, msg = resp['token'], resp['bot'], resp['chat_id'], resp['msg']
+    
+    if authorize(_token):
+        contents = urllib2.urlopen(base_url+bot+"/sendMessage?chat_id="+chat_id+"&text="+msg).read()
+    else:
+        return "invalid token"
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
--- helpers/lead_helper.py
+++ helpers/lead_helper.py
@@ -4,80 +4,83 @@ from helpers.telegram_helper import TelegramHelper
 import datetime
 import calendar
 import time
-from flask import Flask,redirect
+from flask import Flask,redirect, render_template
 
 
 class LeadHelper:
-	def __init__(self):
-		self.db = DBConnection()
-		self.connection = self.db.get_connection()
+    def __init__(self):
+        self.db = DBConnection()
+        self.connection = self.db.get_connection()
 
-	def str_to_bool(self, string):
-	    if string == 'True':
-	         return True
-	    elif string == 'False':
-	         return False
+    def str_to_bool(self, string):
+        if string == 'True':
+             return True
+        elif string == 'False':
+             return False
 
-	def get_file_name(self, filename):
-		ts = calendar.timegm(time.gmtime())
-		filename = str(ts)+filename
-		return filename
+    def get_file_name(self, filename):
+        ts = calendar.timegm(time.gmtime())
+        filename = str(ts)+filename
+        return filename
 
 
-	def insert_lead(self, name, email, necessity, enterprise, role, state, city, phone, celphone, origin, alert):
-		cursor = self.connection.cursor()
-		now = datetime.datetime.utcnow()
-		sql    = "INSERT INTO lead (name, email, necessity, enterprise, role, state, city, phone, celphone, origin, date_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-		val    = (name, email, necessity, enterprise, role, state, city, phone, celphone, origin, now)
-		cursor.execute(sql, val)
-		self.connection.commit()
-		print(cursor.rowcount, "lead inserted.")
-		alert = self.str_to_bool(alert)
-		if(alert):
-			th = TelegramHelper()
-			th.broadcast_alert("Novo+Lead: ")
+    def insert_lead(self, name, email, necessity, enterprise, role, state, city, phone, celphone, origin, alert):
+        cursor = self.connection.cursor()
+        now = datetime.datetime.utcnow()
+        sql    = "INSERT INTO lead (name, email, necessity, enterprise, role, state, city, phone, celphone, origin, date_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val    = (name, email, necessity, enterprise, role, state, city, phone, celphone, origin, now)
+        cursor.execute(sql, val)
+        self.connection.commit()
+        print(cursor.rowcount, "lead inserted.")
+        alert = self.str_to_bool(alert)
+        if(alert):
+            th = TelegramHelper()
+            th.broadcast_alert("Novo+Lead: ")
 
-		return "200"
+        return "200"
 
-	def compute_access(self, origin):
-		cursor = self.connection.cursor()
-		sql    = "SELECT access_amount FROM campaing WHERE origin = '%s'" % origin
-		cursor.execute(sql)
-		amount = cursor.fetchall()
-		amount = amount[0][0]
-		new_amount = int(amount) + 1
-		self.update_acess(origin, new_amount)
+    def compute_access(self, origin):
+        cursor = self.connection.cursor()
+        sql    = "SELECT access_amount FROM campaing WHERE origin = '%s'" % origin
+        cursor.execute(sql)
+        amount = cursor.fetchall()
+        amount = amount[0][0]
+        new_amount = int(amount) + 1
+        self.update_acess(origin, new_amount)
 
-		return str(new_amount)
+        return str(new_amount)
 
-	def update_acess(self, origin, amount):
-		cursor = self.connection.cursor()
-		sql = "UPDATE campaing SET access_amount = '%s' WHERE origin = '%s'" % (amount, origin)
-		cursor.execute(sql)
-		self.connection.commit()
+    def update_acess(self, origin, amount):
+        cursor = self.connection.cursor()
+        sql = "UPDATE campaing SET access_amount = '%s' WHERE origin = '%s'" % (amount, origin)
+        cursor.execute(sql)
+        self.connection.commit()
 
-	def formata_inscricao():
-		pass
+    def formata_inscricao():
+        pass
 
-	def nova_inscricao(self, data, historico, diploma):
-		alert = True
-		cursor = self.connection.cursor()
-		now = datetime.datetime.utcnow()
-		sql    = "INSERT INTO inscricao (nome_completo, data_nascimento, rg, cpf, celular, email, \
-		rua, numero, bairro, estado, cidade, cep, complemento, escolaridade, formacao, foco_aulas, \
-		caminho_historico, caminho_diploma, date_time) \
-		VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    def nova_inscricao(self, data, historico, diploma):
 
-		val    = (data['nome_completo'], data['data_nascimento'], data['rg'], data['cpf'],\
-		data['celular'], data['email'], data['rua'], data['numero'], data['bairro'],\
-		data['estado'], data['cidade'], data['cep'], data['complemento'], data['escolaridade'],\
-		data['formacao'], data['foco_aulas'], self.get_file_name(historico), self.get_file_name(diploma), now)
-		cursor.execute(sql, val)
-		self.connection.commit()
-		print(cursor.rowcount, "inscricao realizada.")
-		if(alert):
-			th = TelegramHelper()
-			msg = "Nova+Inscricao:+Nome:+%s+e-mail+%s" %  (data['nome_completo'], data['email'])
-			th.broadcast_alert(msg)
+        try:
+            alert = True
+            cursor = self.connection.cursor()
+            now = datetime.datetime.utcnow()
+            sql = "INSERT INTO inscricao (nome_completo, data_nascimento, rg, cpf, celular, email, \
+            rua, numero, bairro, estado, cidade, cep, complemento, escolaridade, formacao, foco_aulas, \
+            caminho_historico, caminho_diploma, date_time) \
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
-		return redirect("http://www.mercadopago.com.br", code=302)
+            val = (data['nome_completo'], data['data_nascimento'], data['rg'], data['cpf'],\
+            data['celular'], data['email'], data['rua'], data['numero'], data['bairro'],\
+            data['estado'], data['cidade'], data['cep'], data['complemento'], data['escolaridade'],\
+            data['formacao'], data['foco_aulas'], self.get_file_name(historico), self.get_file_name(diploma), now)
+            cursor.execute(sql, val)
+            self.connection.commit()
+            print(cursor.rowcount, "inscricao realizada.")
+            if(alert):
+                th = TelegramHelper()
+                msg = "Nova+Inscricao:+Nome:+%s+e-mail+%s" %  (data['nome_completo'], data['email'])
+                th.broadcast_alert(msg)
+                return True
+        except:
+                return False
