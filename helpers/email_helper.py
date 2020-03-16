1e33ccad894b13a25bf8280bbe73830d8fe084a3
Fernando Gualberto <fernando.gmp@gmail.com>
2020-03-16 17:03:29 -0300

    adicionando mail server e mail port para variaveis de ambiente

--- helpers/email_helper.py
+++ helpers/email_helper.py
@@ -7,8 +7,8 @@ class EmailHelper:
 
     def __init__(self, app):
         self.mail_settings = {
-            "MAIL_SERVER": 'smtp.gmail.com',
-            "MAIL_PORT": 465,
+            "MAIL_SERVER": os.environ['MAIL_SERVER'],
+            "MAIL_PORT": os.environ['MAIL_PORT'],
             "MAIL_USE_TLS": False,
             "MAIL_USE_SSL": True,
             "MAIL_USERNAME": os.environ['EMAIL_USER'],
