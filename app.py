4aaefd223b45d5119414d1182b99b579bcaa5fe1
Fernando Gualberto <fernando.gmp@gmail.com>
2020-03-18 17:04:22 -0300

    convertendo tabs para espacos

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ CORS(app)
 @app.route("/enviar", methods=['POST'])
 @cross_origin(origin='localhost',headers=['Content-Type'])
 def enviar_email():
-	email = EmailHelper(app)
-	data = request.form
-	data = json.dumps(data)
-	data = json.loads(data)
-	emails, assunto, mensagem = data['email'], data['assunto'], data['mensagem']
+    email = EmailHelper(app)
+    data = request.form
+    data = json.dumps(data)
+    data = json.loads(data)
+    emails, assunto, mensagem = data['email'], data['assunto'], data['mensagem']
 
-	end = []
-	end.append(emails)
-	email.send_email(assunto, end, mensagem)
+    end = []
+    end.append(emails)
+    email.send_email(assunto, end, mensagem)
 
-	return data
-    
+    return data
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- helpers/email_helper.py
+++ helpers/email_helper.py
@@ -24,5 +24,5 @@ class EmailHelper:
                       sender=("EsporteCash", self.app.config.get("MAIL_USERNAME")),
                       recipients=recipients)
         msg.html=render_template('basic-email.html', email_body = body)
-        
+
         self.e_mail.send(msg)
