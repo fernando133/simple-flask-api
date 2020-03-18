3c9052c85fe23e0220c98a6702027be138485036
Fernando Gualberto <fernando.gmp@gmail.com>
2020-03-18 16:54:50 -0300

    remocendo imports não utilizados

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-from flask import request, jsonify
-import urllib2
+from flask import request
 import json
 from helpers.email_helper import EmailHelper
-
-import os
-from flask import Flask, flash, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
-from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
-
-@app.route("/testeEmail", methods=['GET', 'POST'])
-def teste_email():
-    email = EmailHelper(app)
-    dest = "fernando.gmp@gmail.com"
-    dest1 = "desenvolvimento@inlinetech.com.br"
-    recipients=[dest, dest1]
-    email.send_email("Teste", recipients, "Corpo do e-mail.")
-    return "200"
 
 @app.route("/enviar", methods=['POST'])
 @cross_origin(origin='localhost',headers=['Content-Type'])
